0223d3f42ecab5bbf002185fb12f13835d646b2f
thrchkng <maxbog5236@gmail.com>
2025-11-28 23:48:56 +0500

    Delete src/tic_tac_toe/gameparts/exceptions.py
    Нет консоли, нет ошибок для отработки

--- src/tic_tac_toe/gameparts/exceptions.py
+++
@@ -1,8 +0,0 @@
-class FieldIndexError(IndexError):
-    def __str__(self):
-        return 'Выведено значение за границами игрового поля'
-
-
-class CellOccupiedError(Exception):
-    def __str__(self):
-        return 'Попытка исправить усугубила положение'
